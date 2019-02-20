09620dc3d90f7b0538f44a5a894b91cc9514ad51
sebasjara <sebas.jara1@hotmail.com>
2019-02-19 20:49:21 -0500

    Sección 'Team' Creada

--- js/custom.js
+++ js/custom.js
@@ -2,15 +2,40 @@
                     SERVICES
 =========================================================*/
 $(function(){
-    // Animate on scroll
-    new WOW().init();
-
-    // Pop-up gallery
-    $('#work').magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
-        type: 'image',
-        gallery:{enabled:true}
-      });
+    IniciarComponentes();
 });
+
+function IniciarComponentes (){
+     // Animate on scroll
+     new WOW().init();
+
+     // Pop-up gallery
+     $('#work').magnificPopup({
+         delegate: 'a', // child items selector, by clicking on it popup will open
+         type: 'image',
+         gallery:{enabled:true}
+     });
+ 
+     // Carousel gallery
+     $('.owl-carousel').owlCarousel({
+         loop:true,
+         margin:10,
+         nav:true,
+         autoplay: true,
+         smartSpeed: 700,
+         autoplayHoverPause: true,
+         responsive:{
+             0:{
+                 items:1
+             },
+             600:{
+                 items:3
+             },
+             1000:{
+                 items:3
+             }
+         }
+     }); 
+}
 
 
